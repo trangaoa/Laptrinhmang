c6683a7aa20fdda4194dfbb49214a40caa9e0f6e
trangaoa <57657974+trangaoa@users.noreply.github.com>
2022-09-28 12:12:40 +0700

    Update Server.java
    Thêm phần process check Fibonacci

--- src/Server.java
+++ src/Server.java
@@ -205,6 +205,41 @@ public class Server {
         }
     }
 
+    public void processFibonacci() {
+        ServerSocket sever = null;
+        Socket socket = null;
+        CheckFibonacci checkFibonacci = null;
+        try {
+            sever = new ServerSocket(Server.PORT);
+
+            System.out.println("Waiting for new connection....");
+
+            socket = sever.accept();
+
+            String seed = PKT_HELLO(socket);
+
+            ArrayList<Integer> data = new ArrayList<>();
+            ArrayList<Integer> res = new ArrayList<>();
+            checkFibonacci = new CheckFibonacci();
+
+            for (int i : checkFibonacci.getMyArr())
+                data.add(i);
+                checkFibonacci.count();
+            for (int i : checkFibonacci.getMyArr())
+                res.add(i);
+
+            seed += data.size();
+
+            PKT_CALC(socket, data);
+
+            PKT_RES(socket, seed, res);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
     public static void main(String[] args) {
         Server server = new Server();
         server.process();
